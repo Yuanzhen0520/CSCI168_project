f06870d5966f393ed6cdf3cadbfbc5fdb6216d19
Yuanzhen0520 <yzhang18@scu.edu>
2021-11-18 21:50:45 -0800

    Update light.h

--- light.h
+++ light.h
@@ -5,11 +5,15 @@
 
 class Light{
   public:
-    Light() {}; 
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    glm::vec3 direciton;
+    Light() {};
     glm::vec3 getPosition() { return position; }
     Light(glm::vec3 position) : position(position) {}
     void setPosition(glm::vec3 p) { position = p;}
-  
+
   private:
     glm::vec3 position;
 };
