43886b15911e6c1327d4822c2b3a1aa33cc5209e
hkareti <63679161+hkareti@users.noreply.github.com>
2021-11-18 22:41:18 -0800

    Create light.cpp

---
+++ light.cpp
@@ -0,0 +1,20 @@
+#include "light.h"
+
+glm:vec3 Light::multipleLights(Material_m, glm::vec3_p,glm::vec3_n,glm:::vec3_d,glm.vec3_cEye){
+  //ambient light
+  
+  glm::vec3IA;
+  IA = _m.kA* iA
+    
+    //Lambertian Shading 
+  glm:vec3 ID; 
+  ID = _m.kD * iD * std::max(0,glm::dot(_n,_d));
+  
+  //Blinn-Phong Shading 
+  glm::vec3 IS
+  glm:: vec3 h; 
+  h = glm::normalize(c.Eye -_p)+ _d);
+  IS = _m.kS * iS * pow(std::max(0,glm::dot(_cEye,h)),_m.shininess);
+  
+  return IA +ID +IS
+  }
